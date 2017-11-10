8f5ebdcdd582680edfd8f3789fbef5f9ed3b1397
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-11-09 23:06:03 -0500

    Removed excess using statements from Category enum

--- PokemonGeneration1/Source/Battles/Category.cs
+++ PokemonGeneration1/Source/Battles/Category.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PokemonGeneration1.Source.Battles
+﻿namespace PokemonGeneration1.Source.Battles
 {
     public enum Category
     {
